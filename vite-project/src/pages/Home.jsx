86d0a9c4c1129f9ff4ec6fdaecfba8015c9bc374
huyvu38 <vu38@purdue.edu>
2025-07-03 02:00:42 +0700

    Fix error

--- vite-project/src/pages/Home.jsx
+++ vite-project/src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import { Navbar } from "../components/Navbar";
+import { Navbar } from "../components/NavBar";
 import { ThemeToggle } from "../components/ThemeToggle";
 import { HeroSection } from "../components/HeroSection";
 import { SkillsSection } from "../components/SkillsSection";
@@ -8,7 +8,7 @@ export const Home = () => {
   return (
     <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
       <ThemeToggle />
-
+      <Navbar />
       <main>
         <HeroSection />
         <SkillsSection />
