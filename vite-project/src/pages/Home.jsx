3b01b3cbd504e17b675069a38aafb2aa32aa7059
huyvu38 <vu38@purdue.edu>
2025-07-03 01:53:51 +0700

    Fix error

--- vite-project/src/pages/Home.jsx
+++ vite-project/src/pages/Home.jsx
@@ -8,7 +8,7 @@ export const Home = () => {
   return (
     <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
       <ThemeToggle />
-      <Navbar />
+
       <main>
         <HeroSection />
         <SkillsSection />
